d8233e81811bf2c5b491ee214bc80bf9c6c2bd2a
oshanedesign <oshane.design@gmail.com>
2022-03-23 22:55:21 -0500

    no luck

--- src/deleteATask.js
+++ src/deleteATask.js
@@ -4,8 +4,9 @@ import { addListListener } from "./addNewTask";
 export const deleteItemListener = (element) => { 
     // const iconContainer = document.querySelector('.icon');
      element.addEventListener('click', () => {
-        let binIcon = element.innerHTML = `<i class="fa-solid fa-trash-can"></i>`;
-    }), binIcon.addEventListener('click', () => {
+        const binIcon = element.innerHTML = `<i class="fa-solid fa-trash-can"></i>`;
+    }), 
+    binIcon.addEventListener('click', () => {
         // when i click binicon it should delete that list 
         console.log('Hellow world!!!')
     });;
